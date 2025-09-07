843472066b6d635717cfdebfe70d645f823b1634
~ Gui Correia <103298767+CorreiaGui@users.noreply.github.com>
2025-09-07 19:16:19 -0300

    create utils

---
+++ src/main/java/br/com/fiap/qhealth/utils/QHealthConstants.java
@@ -0,0 +1,9 @@
+package br.com.fiap.qhealth.utils;
+
+public class QHealthConstants {
+
+    private QHealthConstants(){}
+
+    public static final String ID_INVALIDO = "ID inválido.";
+    public static final String ID_NAO_ENCONTRADO = "ID não encontrado.";
+}
---
+++ src/main/java/br/com/fiap/qhealth/utils/QHealthUtils.java
@@ -0,0 +1,25 @@
+package br.com.fiap.qhealth.utils;
+
+import br.com.fiap.qhealth.model.Paciente;
+import org.modelmapper.ModelMapper;
+
+import java.util.UUID;
+
+import static br.com.fiap.qhealth.utils.QHealthConstants.ID_INVALIDO;
+import static java.util.regex.Pattern.matches;
+
+public class QHealthUtils {
+
+    private static final String REGEX_UUID = "^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$";
+
+    public static void uuidValidator(UUID id) {
+        if (!matches(REGEX_UUID, id.toString())) {
+            throw new ResourceNotFoundException(ID_INVALIDO);
+        }
+    }
+
+    public static Paciente convertToPaciente(PacienteBodyRequest pacienteRequest) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(pacienteRequest, Paciente.class);
+    }
+}
